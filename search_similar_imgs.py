6b79c191cad3a9ea21ce7f78fc4e5c9b104d5c2d
Eleonora Renz <eleonora.renz96@web.de>
2022-03-31 23:39:39 +0200

    Update search func

--- search_similar_imgs.py
+++ search_similar_imgs.py
@@ -3,11 +3,20 @@
 
 import sys
 import searchimagelibrary as sil
+import pandas as pd
 
 input_img_path = sys.argv[1]
 input_img = sil.pathsToImages(input_img_path)
 
+# Calculate
 color_hist_input = sil.colorHistogramFromPath(input_img)
 gabor_vec_input = sil.processGaborVectorFromPath(input_img_path)
 
 match_results = sil.colorPlusTextureMatch(input_img, color_hist_input, num_results=5)
+
+img_feature_df = pd.read_csv(f'img_library_color_texture_features.csv')
+parasite = img_feature_df.iloc[666]['parasite']
+
+# Display search image and results
+sil.show_images(input_img, figsize=(8, 5), title=f"Search Query - {parasite}")
+sil.show_images(match_results['path'], title="Correlation", columns=len(match_results))
