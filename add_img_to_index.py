9e2b81995febca749c5c85b0b31fe1cd72a5ef46
Eleonora Renz <eleonora.renz96@web.de>
2022-03-31 22:25:29 +0200

    Improve indexing and searching

--- add_img_to_index.py
+++ add_img_to_index.py
@@ -1,10 +1,11 @@
+#Please provide the full path to the img you want to add to the index as follows:
+#>>python add_img_to_index.py **path-to-your-img**
 import sys
 import os
 
-print("Please provide the full path to the img you want to add to the index as follows:")
-print(">>python add_img_to_index.py **path-to-your-img**")
 img_to_add = sys.argv[1]
 img_to_add_filename = str(img_to_add).split('/')[-1]
-img_bank_path = '../sample_images/'
+img_bank_path = 'sample_images/'
 
 os.replace(img_to_add, img_bank_path+img_to_add_filename)
+# TODO calculate color and texture features and store new csv
--- search_similar_imgs.py
+++ search_similar_imgs.py
@@ -1,9 +1,12 @@
+#Please provide the full path to the img you want to search against as follows:
+#>>python add_img_to_index.py **path-to-your-img**
+
 import sys
 import searchimagelibrary as sil
-
-print("Please provide the full path to the img you want to search against as follows:")
-print(">>python add_img_to_index.py **path-to-your-img**")
 
 input_img = sys.argv[1]
 
 color_hist_input = sil.colorHistogramFromPath(input_img)
+color_match = sil.colorMatch(input_img, color_hist_input)
+
+#TODO calculate texture stuff and 
